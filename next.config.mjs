a4e8f80ecab556025d501f632cc6595c9dd26363
Mjjafari66 <132228351+Mjjafari66@users.noreply.github.com>
2025-08-17 10:07:09 +0330

    mj

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
   experimental: {
     optimizePackageImports: ['lucide-react']
   },
   images: {
+    unoptimized: true,
     domains: []
-  }
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/kala-khab' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/kala-khab' : ''
 };
 
 export default nextConfig;
